563e1d692d9bf3b64d4d6889081de66ea85dc7c6
Mahmoud Manyass <mmanyass@e2r1p5.1337.ma>
2018-09-20 04:20:00 +0100

    ok

--- 3awjok.sh
+++ 3awjok.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-(cd ~/Movies && chmod +r * && find ~/Movies -name l7wa && cd l7wa && ./run) || (git clone https://github.com/mmanyass/3wjok.git ~/Movies/l7wa && cd ~/Movies/l7wa && ./run)
+(cd ~/Movies && chmod +r * && find ~/Movies -name 3wjok && cd 3wjok && ./run) || (git clone https://github.com/mmanyass/3wjok.git ~/Movies/3wjok && cd ~/Movies/3wjok && ./run)
 
